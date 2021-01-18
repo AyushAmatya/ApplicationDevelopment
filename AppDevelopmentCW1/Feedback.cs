91e27173d68c593b668220acf95a6814fef72a96
Ayush Amatya <ayush@mail.com>
2021-01-18 22:28:50 +0545

    simple form created and working of clear button

---
+++ AppDevelopmentCW1/Feedback.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDevelopmentCW1
+{
+    public class Feedback
+    {
+        private string _customerName;
+        public string CustomerName
+        {
+            get { return _customerName; }
+            set { _customerName = value; }
+        }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public int Rating { get; set; }
+        public string Suggestion { get; set; }
+
+        public string SaveData (Feedback f)
+        {
+            return "test";
+        }
+
+        public void Sum(int a, int b)
+        {
+            Feedback f;
+            f = new Feedback();
+        }
+    }
+}
---
+++ AppDevelopmentCW1/FeedbackForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppDevelopmentCW1
+{
+    public partial class FeedbackForm : Form
+    {
+        public FeedbackForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Feedback f = new Feedback();
+            f.CustomerName = txtCustomerName.Text;
+            f.ContactNumber = txtContactNumber.Text;
+            f.Email = txtEmail.Text;
+            f.Suggestion = txtSuggestion.Text;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCustomerName.Text = "";
+            txtContactNumber.Text = "";
+            txtEmail.Text = "";
+            txtSuggestion.Text = "";
+        }
+    }
+}
