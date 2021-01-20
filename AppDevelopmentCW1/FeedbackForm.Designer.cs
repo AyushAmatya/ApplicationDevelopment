6bf05e1f8959489b924cb94a1f3ff0194f241bf2
Ayush Amatya <amatyahome@gmail.com>
2021-01-20 10:13:56 +0545

    sfdg

--- AppDevelopmentCW1/FeedbackForm.Designer.cs
+++ AppDevelopmentCW1/FeedbackForm.Designer.cs
@@ -40,36 +40,10 @@ namespace AppDevelopmentCW1
             this.btnSave = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.lblLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.radioButton7 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton8 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            this.panel6.SuspendLayout();
-            this.panel5.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblCustomerName
@@ -184,89 +158,26 @@ namespace AppDevelopmentCW1
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // panel1
-            // 
-            this.panel1.AutoScroll = true;
-            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.panel1.Controls.Add(this.splitContainer1);
-            this.panel1.Location = new System.Drawing.Point(58, 196);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(673, 184);
-            this.panel1.TabIndex = 14;
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.splitContainer1.Panel1.Controls.Add(this.panel6);
-            this.splitContainer1.Panel1.Controls.Add(this.panel5);
-            this.splitContainer1.Panel1.Controls.Add(this.lblLabel);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.splitContainer1.Panel2.Controls.Add(this.label2);
-            this.splitContainer1.Panel2.Controls.Add(this.panel4);
-            this.splitContainer1.Panel2.Controls.Add(this.panel2);
-            this.splitContainer1.Size = new System.Drawing.Size(673, 184);
-            this.splitContainer1.SplitterDistance = 216;
-            this.splitContainer1.TabIndex = 0;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel6.Controls.Add(this.label3);
-            this.panel6.Location = new System.Drawing.Point(3, 75);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(210, 24);
-            this.panel6.TabIndex = 20;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(8, 2);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(137, 20);
-            this.label3.TabIndex = 16;
-            this.label3.Text = "Staff Friendliness:";
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel5.Controls.Add(this.label1);
-            this.panel5.Location = new System.Drawing.Point(3, 42);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(210, 24);
-            this.panel5.TabIndex = 19;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(8, 1);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 20);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "Food Quality:";
-            // 
             // lblLabel
             // 
             this.lblLabel.AutoSize = true;
             this.lblLabel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.lblLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblLabel.Location = new System.Drawing.Point(7, 5);
+            this.lblLabel.Location = new System.Drawing.Point(70, 193);
             this.lblLabel.Name = "lblLabel";
             this.lblLabel.Size = new System.Drawing.Size(92, 24);
             this.lblLabel.TabIndex = 17;
             this.lblLabel.Text = "Criterias:";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(58, 228);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 152);
+            this.panel1.TabIndex = 14;
             // 
             // label2
             // 
@@ -274,153 +185,19 @@ namespace AppDevelopmentCW1
             this.label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(8, 5);
+            this.label2.Location = new System.Drawing.Point(294, 193);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 24);
             this.label2.TabIndex = 18;
             this.label2.Text = "Ratings:";
             // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.button2);
-            this.panel4.Controls.Add(this.radioButton7);
-            this.panel4.Controls.Add(this.radioButton1);
-            this.panel4.Controls.Add(this.radioButton8);
-            this.panel4.Controls.Add(this.radioButton6);
-            this.panel4.Location = new System.Drawing.Point(3, 72);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(433, 24);
-            this.panel4.TabIndex = 1;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(386, 1);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(43, 21);
-            this.button2.TabIndex = 20;
-            this.button2.Text = "Clear";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // radioButton7
-            // 
-            this.radioButton7.AutoSize = true;
-            this.radioButton7.Location = new System.Drawing.Point(199, 3);
-            this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(65, 17);
-            this.radioButton7.TabIndex = 17;
-            this.radioButton7.TabStop = true;
-            this.radioButton7.Text = "Average";
-            this.radioButton7.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(118, 4);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(51, 17);
-            this.radioButton1.TabIndex = 19;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Good";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton8
-            // 
-            this.radioButton8.AutoSize = true;
-            this.radioButton8.Location = new System.Drawing.Point(17, 3);
-            this.radioButton8.Name = "radioButton8";
-            this.radioButton8.Size = new System.Drawing.Size(68, 17);
-            this.radioButton8.TabIndex = 16;
-            this.radioButton8.TabStop = true;
-            this.radioButton8.Text = "Excellent";
-            this.radioButton8.UseVisualStyleBackColor = true;
-            // 
-            // radioButton6
-            // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(291, 3);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(78, 17);
-            this.radioButton6.TabIndex = 18;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "Dissatisfied";
-            this.radioButton6.UseVisualStyleBackColor = true;
-            // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.radioButton5);
-            this.panel2.Controls.Add(this.radioButton4);
-            this.panel2.Controls.Add(this.radioButton3);
-            this.panel2.Controls.Add(this.radioButton2);
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(3, 42);
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Location = new System.Drawing.Point(58, 187);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(433, 24);
-            this.panel2.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(386, 1);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(43, 21);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "Clear";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(118, 4);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(51, 17);
-            this.radioButton5.TabIndex = 5;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Good";
-            this.radioButton5.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(291, 3);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(78, 17);
-            this.radioButton4.TabIndex = 4;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Dissatisfied";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(199, 3);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(65, 17);
-            this.radioButton3.TabIndex = 3;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Average";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(17, 3);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(68, 17);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Excellent";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(0, 30);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(433, 24);
-            this.panel3.TabIndex = 1;
+            this.panel2.Size = new System.Drawing.Size(673, 35);
+            this.panel2.TabIndex = 19;
             // 
             // FeedbackForm
             // 
@@ -428,6 +205,8 @@ namespace AppDevelopmentCW1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblLabel);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnClear);
@@ -440,23 +219,9 @@ namespace AppDevelopmentCW1
             this.Controls.Add(this.lblContactNumber);
             this.Controls.Add(this.txtCustomerName);
             this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.panel2);
             this.Name = "FeedbackForm";
             this.Text = "FeedbackForm";
-            this.panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.panel6.ResumeLayout(false);
-            this.panel6.PerformLayout();
-            this.panel5.ResumeLayout(false);
-            this.panel5.PerformLayout();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -475,26 +240,9 @@ namespace AppDevelopmentCW1
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnBack;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label lblLabel;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RadioButton radioButton7;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton8;
-        private System.Windows.Forms.RadioButton radioButton6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
--- AppDevelopmentCW1/FeedbackForm.cs
+++ AppDevelopmentCW1/FeedbackForm.cs
@@ -24,8 +24,7 @@ namespace AppDevelopmentCW1
             //int locationX = 50;
             //int locationY = 10;
 
-            int locationX = 12;
-            int locationY = 100;
+            int locationY = 5;
             string line;
             System.IO.StreamReader file =
                 new System.IO.StreamReader(@"criteria.txt");
@@ -34,39 +33,94 @@ namespace AppDevelopmentCW1
                 // Remove the extra ','
                 string comboName = line.Substring(0, line.Length - 1);
 
-                ComboBox comboBox = new ComboBox();
-                comboBox.Name = comboName;
-                comboBox.Items.AddRange(new object[] { 1, 2, 3, 4 });
-                comboBox.Location = new Point(locationX, locationY);
-                //this.Controls.Add(comboBox);
-                //this.splitContainer1.Panel1.Controls.Add(comboBox);
 
-
-
-                //Label label0 = new Label();
-                //label0.Text = comboName;
-                //label0.Location = new Point(0, locationY);
-                //this.Controls.Add(label);
-                //this.splitContainer1.Panel1.Controls.Add(label0);
+                RadioButton radioButton1 = new RadioButton();
 
                 Label label10 = new Label();
                 label10.Text = comboName;
                 label10.Location = new Point(8, 1);
 
+                label10.AutoSize = true;
+                label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                label10.Size = new System.Drawing.Size(102, 20);
+                label10.TabIndex = 15;
 
                 Panel panel10 = new Panel();
                 panel10.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
                 panel10.Size = new System.Drawing.Size(210, 24);
-                panel10.Location = new Point(3, locationY);
+                panel10.Location = new Point(10, locationY);
                 panel10.Controls.Add(label10);
-                this.splitContainer1.Panel1.Controls.Add(panel10);
-                locationY += 20;
+                this.panel1.Controls.Add(panel10);
+
+                Button button11 = new Button();
+                button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+                button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button11.Location = new Point(386, 1);
+                button11.Size = new System.Drawing.Size(43, 21);
+                button11.TabIndex = 15;
+                button11.Text = "Clear";
+                button11.UseVisualStyleBackColor = false;
+
+                RadioButton radioButton15 = new RadioButton();
+                radioButton15.AutoSize = true;
+                radioButton15.Location = new System.Drawing.Point(118, 4);
+                radioButton15.Name = "radioButton5";
+                radioButton15.Size = new System.Drawing.Size(51, 17);
+                radioButton15.TabIndex = 5;
+                radioButton15.TabStop = true;
+                radioButton15.Text = "Good";
+                radioButton15.UseVisualStyleBackColor = true;
+
+                RadioButton radioButton14 = new RadioButton();
+                radioButton14.AutoSize = true;
+                radioButton14.Location = new Point(291, 3);
+                radioButton14.Name = "radioButton4";
+                radioButton14.Size = new System.Drawing.Size(78, 17);
+                radioButton14.TabIndex = 4;
+                radioButton14.TabStop = true;
+                radioButton14.Text = "Dissatisfied";
+                radioButton14.UseVisualStyleBackColor = true;
+
+                RadioButton radioButton13 = new RadioButton();
+                radioButton13.AutoSize = true;
+                radioButton13.Location = new Point(199, 3);
+                radioButton13.Name = "radioButton3";
+                radioButton13.Size = new System.Drawing.Size(65, 17);
+                radioButton13.TabIndex = 3;
+                radioButton13.TabStop = true;
+                radioButton13.Text = "Average";
+                radioButton13.UseVisualStyleBackColor = true;
+
+                RadioButton radioButton12 = new RadioButton();
+                radioButton12.AutoSize = true;
+                radioButton12.Location = new Point(17, 3);
+                radioButton12.Name = "radioButton2";
+                radioButton12.Size = new System.Drawing.Size(68, 17);
+                radioButton12.TabIndex = 2;
+                radioButton12.TabStop = true;
+                radioButton12.Text = "Excellent";
+                radioButton12.UseVisualStyleBackColor = true;
+
+                Panel panel11 = new Panel();
+                panel11.SuspendLayout();
+                panel11.Controls.Add(button11);
+                panel11.Controls.Add(radioButton15);
+                panel11.Controls.Add(radioButton14);
+                panel11.Controls.Add(radioButton13);
+                panel11.Controls.Add(radioButton12);
+                //panel11.Controls.Add(this.panel3);
+                panel11.Location = new Point(220, locationY);
+                panel11.Size = new System.Drawing.Size(433, 24);
+                panel11.TabIndex = 0;
+                panel11.ResumeLayout(false);
+                panel11.PerformLayout();
+                this.panel1.Controls.Add(panel11);
                 //private System.Windows.Forms.Label label1;
 
 
                 //private System.Windows.Forms.Panel panel5;
 
-
+                locationY += 30;
             }
 
             file.Close();
